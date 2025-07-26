935fc14f6b01a293dff2b29a2bef2803b40307a9
rumahkodingku <rumahkodingku45@gmail.com>
2025-07-26 23:10:54 +0700

    style: update ui for news section

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,7 +1,7 @@
 import { Footer } from '@/components/footer';
 import GallerySectionV3 from '@/components/gallery-section/gallery-section3';
 import HeroSectionV3 from '@/components/hero-section/hero-section3';
-import NavbarV2 from '@/components/navbar/navbar2';
+import NavbarV3 from '@/components/navbar/navbar3';
 import NewsSectionV3 from '@/components/news-section/news-section3';
 import ServiceSectionV2 from '@/components/service-section/service-section2';
 import StatisticSectionV3 from '@/components/statistic-section/statistic-section3';
@@ -10,7 +10,7 @@ import VideoSectionV3 from '@/components/video-section/video-section3';
 export default function Welcome() {
     return (
         <>
-            <NavbarV2 />
+            <NavbarV3 />
             <HeroSectionV3 />
             <ServiceSectionV2 />
             <StatisticSectionV3 />
