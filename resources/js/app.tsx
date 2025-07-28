97c27b38551aa30719112d53de816d1802848534
rumahkodingku <rumahkodingku45@gmail.com>
2025-07-28 09:29:42 +0700

    feat: install sonner

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,17 +3,23 @@ import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from './components/ui/sonner';
 import { initializeTheme } from './hooks/use-appearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => title ? `${title} - ${appName}` : appName,
+    title: (title) => (title ? `${title} - ${appName}` : appName),
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <main>
+                <App {...props} />
+                <Toaster />
+            </main>,
+        );
     },
     progress: {
         color: '#4B5563',
