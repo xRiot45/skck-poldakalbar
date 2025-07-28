84659d8f719c99681319cf171090963065a6898b
rumahkodingku <rumahkodingku45@gmail.com>
2025-07-28 13:57:51 +0700

    feat: generate resource vision

---
+++ app/Models/Vision.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vision extends Model
+{
+    protected $table = 'visions';
+
+    protected $fillable = ['title'];
+}
