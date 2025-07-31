0cb5c6b516a44f223d221dca632be949c0d0c4d2
rumahkodingku <rumahkodingku45@gmail.com>
2025-07-31 08:50:07 +0700

    feat: setup database, model & controller

---
+++ app/Http/Controllers/VideoCategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VideoCategory;
+use Illuminate\Http\Request;
+
+class VideoCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VideoCategory $videoCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VideoCategory $videoCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, VideoCategory $videoCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VideoCategory $videoCategory)
+    {
+        //
+    }
+}
