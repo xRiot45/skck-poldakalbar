d2a81b1dc71abf9320617a067051ce0a997b6d17
rumahkodingku <rumahkodingku45@gmail.com>
2025-08-01 09:13:16 +0700

    feat: setup table, model & controller news category

---
+++ app/Models/NewsCategory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class NewsCategory extends Model
+{
+    protected $table = 'news_categories';
+
+    protected $fillable = ['name', 'slug'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->slug = Str::slug($model->name);
+        });
+
+        static::updating(function ($model) {
+            $model->slug = Str::slug($model->name);
+        });
+    }
+}
