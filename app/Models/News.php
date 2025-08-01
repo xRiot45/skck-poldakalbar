cc3aea6d66e23248a07ff1b0bf7435aaca782a36
rumahkodingku <rumahkodingku45@gmail.com>
2025-08-01 09:59:36 +0700

    feat: setup database, model & controller

--- app/Models/News.php
+++ app/Models/News.php
@@ -2,9 +2,48 @@
 
 namespace App\Models;
 
+use App\Enums\NewsStatusEnum;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class News extends Model
 {
-    //
+    protected $table = 'news';
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'thumbnail',
+        'news_category_id',
+        'status',
+        'is_highlight',
+        'views',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $casts = [
+        'is_highlight' => 'boolean',
+        'status' => NewsStatusEnum::class
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->slug = Str::slug($model->title);
+        });
+
+        static::updating(function ($model) {
+            $model->slug = Str::slug($model->title);
+        });
+    }
+
+    public function newsCategory(): BelongsTo
+    {
+        return $this->belongsTo(NewsCategory::class);
+    }
 }
