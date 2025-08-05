e74cb7eb9c8cf9c6b3abc9d31ebc0bc11c0d7888
rumahkodingku <rumahkodingku45@gmail.com>
2025-08-05 09:24:57 +0700

    refactor: refactoring code

--- resources/js/pages/user/pages/skck/index.tsx
+++ resources/js/pages/user/pages/skck/index.tsx
@@ -1,7 +1,6 @@
-import { Footer } from '@/components/footer';
-import NavbarV3 from '@/components/navbar/navbar3';
 import ProsedurSKCKModal from '@/components/prosedur-skck-modal';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import UserLayout from '@/layouts/user';
 import { Icon } from '@iconify/react';
 import { motion } from 'framer-motion';
 
@@ -76,8 +75,7 @@ export default function SkckPage() {
     ];
 
     return (
-        <>
-            <NavbarV3 />
+        <UserLayout>
             <section className="bg-background text-foreground dark:bg-gray-900/90">
                 {/* HERO */}
                 <div className="relative overflow-hidden bg-gray-950 text-white">
@@ -212,7 +210,6 @@ export default function SkckPage() {
                     </div>
                 </div>
             </section>
-            <Footer />
-        </>
+        </UserLayout>
     );
 }
